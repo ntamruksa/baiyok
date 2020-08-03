353d0a4022df28a9bb6c554f9ad5bd052110710a
Niss Tamruksa <ptamruksa@gmail.com>
2020-08-03 11:45:26 +1000

    change logo

--- components/Header.js
+++ components/Header.js
@@ -2,16 +2,16 @@ import Nav from './Nav'
 
 const Header = () => (
   <div className='header'>
-    <div className='header__logo-box'>
+    {/* <div className='header__logo-box'>
       <img src='/Logo-Baiyok-med.png' alt='Logo' className='header__logo' />
-    </div>
-    <div className='header__text-box'>
+    </div> */}
+    {/* <div className='header__text-box'>
       <h1 className='heading-primary'>
         <span className='heading-primary--main'>Bai Yok</span>
         <span className='heading-primary--est'>Est. 2001</span>
         <span className='heading-primary--sub'>Modern Thai Cuisine</span>
       </h1>
-    </div>
+    </div> */}
     <div className='header__menu'>
       <Nav />
     </div>
